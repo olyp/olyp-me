ba6d0fcf3beb8facfe1f88dbfdfb4011303b2f57
August Lilleaas <august@augustl.com>
2014-12-30 02:22:57 +0100

    English.

--- src/olyp_me/web_handlers/login_handler.clj
+++ src/olyp_me/web_handlers/login_handler.clj
@@ -14,18 +14,18 @@
           (link/bundle-paths req ["app.css"]))]
     [:body
      [:div.container
-      [:h1 "Velkommen til tidenes styggeste innloggingsside!"]
+      [:h1 "Olyp login"]
       [:form {:method "POST" :action "/login" :class "form-horizontal"}
        [:input {:type "hidden" :name "__anti-forgery-token" :value (:anti-forgery-token req)}]
        [:div.form-group
-        [:label  {:for "login_username" :class "col-sm-2 control-label"} "Brukernavn"]
+        [:label  {:for "login_username" :class "col-sm-2 control-label"} "Username (e-mail)"]
         [:div.col-sm-4
          [:input {:type "text" :name "username" :id "login_username" :class "form-control"}]]]
        [:div.form-group
-        [:label  {:for "login_password" :class "col-sm-2 control-label"} "Passord"]
+        [:label  {:for "login_password" :class "col-sm-2 control-label"} "Password"]
         [:div.col-sm-4
          [:input {:type "password" :name "password" :id "login_password" :class "form-control"}]]]
-       [:input {:type "submit" :value "Logg inn" :class "btn btn-primary"}]]]])})
+       [:input {:type "submit" :value "Log in" :class "btn btn-primary"}]]]])})
 
 (defn perform-login [{{username "username" password "password"} :form-params {:keys [api-ctx]} :olyp-env}]
   (central-api-client/handle-res
