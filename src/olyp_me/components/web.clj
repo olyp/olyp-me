98ce101ba2b0719b594cecbcd14f2ae9328bc15d
August Lilleaas <august@augustl.com>
2015-03-30 18:11:21 +0200

    Can specify IP

--- src/olyp_me/components/web.clj
+++ src/olyp_me/components/web.clj
@@ -5,7 +5,7 @@
             olyp-me.web-handler)
   (:import [java.util.concurrent TimeUnit]))
 
-(defrecord Web [port]
+(defrecord Web [ip port]
   component/Lifecycle
 
   (start [component]
@@ -13,7 +13,8 @@
                    {:env (:env component)
                     :olyp-central-api-client-ctx (-> component :olyp-central-api-client-ctx)
                     :cookie-secret (:cookie-secret component)})
-          server (org.httpkit.server/run-server handler {:port port})]
+          server (org.httpkit.server/run-server handler {:port port
+                                                         :ip (or ip "0.0.0.0")})]
       (log/info (str "Started web server on port " port))
       (assoc component
         :server server)))
@@ -21,5 +22,5 @@
     ((:server component) :timeout (.convert TimeUnit/MILLISECONDS 1 TimeUnit/SECONDS))
     (dissoc component :server)))
 
-(defn create-web [{:keys [port]}]
-  (Web. port))
+(defn create-web [{:keys [ip port]}]
+  (Web. ip port))
